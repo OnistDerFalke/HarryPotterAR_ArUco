2fc786d47af7c2ecb277d83802c2ae6fc8d72373
Marcin Myśliborski <75864407+OnistDerFalke@users.noreply.github.com>
2024-03-02 16:45:58 +0100

    rotations + fixed aspect ratio

--- Assets/OpenCV+Unity/Demo/Marker_Detector/MarkerDetector.cs
+++ Assets/OpenCV+Unity/Demo/Marker_Detector/MarkerDetector.cs
@@ -15,6 +15,9 @@
 	{
 		public RawImage rawimage;
 		public GameObject model;
+		public RectTransform rawTransform;
+
+		public Camera cam;
 
 		public Texture2D texture;
 
@@ -32,6 +35,11 @@
 		private Texture2D outputTexture;
 		private void Start()
         {
+			var aspectRatio = Screen.height / 600f;
+			
+			rawTransform.localScale = 
+				new Vector3((float)Screen.height / 600f, (float)Screen.height / 600f, 1f);
+
 			// Create default parameres for detection
 			detectorParameters = DetectorParameters.Create();
 
@@ -80,29 +88,20 @@
 
 		void DrawModel(int id)
         {
-			Debug.Log(corners[id][0] + " " + corners[id][1] + " " + corners[id][2] + " " + corners[id][3]);
 			Point2f center = corners[id][0] + corners[id][1] + corners[id][2] + corners[id][3];
-			Point3d center3D = new Point3d(center.X / 4, center.Y / 4, 0);
-
+			Point3d center3D = new Point3d(center.X / 4f, center.Y / 4f, 0);
+			
 			var rvec = new double[] { 0, 0, 0 };
 			var tvec = new double[] { 0, 0, 0 };
 
-			var cameraMatrix = new double[3, 3]
-			{
-				{ 1.6794270741269229e+03, 0.0f, 9.5502638499315810e+02 },
-				{ 0.0f, 1.6771700842601067e+03, 5.5015085362115587e+02 },
-				{ 0.0f, 0.0f, 1.0f }
-			};
-			var dist = new double[] { 1.9683061388899192e-01, -7.3624850611674697e-01,
-				1.4290920850579334e-04, 7.8329305994069088e-04, 5.4742631511243833e-01 };
-
-			//var objPts = new Point3f[]
-			//{
-			//	new Point3f(0,1,0),
-			//	new Point3f(1,1,0),
-			//	new Point3f(1,0,0),
-			//	new Point3f(0,0,0)
-			//};
+            var cameraMatrix = new double[3, 3]
+            {
+                { 1.6794270741269229e+03, 0.0f, 9.5502638499315810e+02 },
+                { 0.0f, 1.6771700842601067e+03, 5.5015085362115587e+02 },
+                { 0.0f, 0.0f, 1.0f }
+            };
+            var dist = new double[] { 1.9683061388899192e-01, -7.3624850611674697e-01,
+                1.4290920850579334e-04, 7.8329305994069088e-04, 5.4742631511243833e-01 };
 
 			var objPts = new Point3f[]
 			{
@@ -112,36 +111,45 @@
 				new Point3f(1,0,0)
 			};
 
-			//Cv2.CalibrateCamera
-
-			//double[,] jacobian;
-			//Point2f[] imgPts;
-			//Cv2.ProjectPoints(objPts, rvec, tvec, cameraMatrix, dist, out imgPts, out jacobian);
 			Cv2.SolvePnP(objPts, corners[id], cameraMatrix, dist, out rvec, out tvec);
-
-			//Debug.Log($"{rvec[0]}, {rvec[1]}, {rvec[2]} {tvec[0]}, {tvec[1]}, {tvec[2]}");
-
 			CvAruco.DrawAxis(mat, cameraMatrix, dist, rvec, tvec, 0.5f);
 			
-			model.transform.localPosition = Point3dToVector3(GetWorldPoint(center3D, rvec, tvec));
-		}
-
-		private Point3d GetWorldPoint(Point3d input, double[] rvec, double[] tvec)
-		{
-			var rot_mat = new Mat();
-			Cv2.Rodrigues(MatOfDouble.FromArray(rvec[0], rvec[1], rvec[2]), rot_mat);
-
-			var pointProject = (rot_mat.Transpose() * MatOfDouble.FromArray(input.X, input.Y, input.Z)).ToMat();
+			//position
+			Vector3 localpos;
+			localpos.x = (float)tvec[0];
+			localpos.y = -(float)tvec[1];
+			localpos.z = (float)tvec[2];
 			
-			return new Point3d(tvec[0], tvec[1], tvec[2]) + 
-				new Point3d(pointProject.At<double>(0, 0), pointProject.At<double>(0, 1), pointProject.At<double>(0, 2));
-		}
+			Vector3 worldpos = cam.transform.TransformPoint(localpos);
+            model.transform.position = worldpos;
 
-		private Vector3 Point3dToVector3(Point3d p)
-        {
-			//Point3d p3d = new Point3d(-100, 250, -340);
-			//return new Vector3((float)p3d.X, (float)p3d.Y, (float)p3d.Z);
-			return new Vector3((float)p.X, (float)p.Y, (float)p.Z);
-        }
+			//rotation
+			double[] flip = rvec;
+			flip[1] = -flip[1];
+			rvec = flip;
+			
+			Mat rotmatrix = new Mat();			
+			MatOfDouble rvecMat = new MatOfDouble(1, 3);
+			rvecMat.Set<double>(0, 0, rvec[0]);
+			rvecMat.Set<double>(0, 1, rvec[1]);
+			rvecMat.Set<double>(0, 2, rvec[2]);
+			Cv2.Rodrigues(rvecMat, rotmatrix);
+
+			Vector3 forward;
+			forward.x = (float)rotmatrix.At<double>(2, 0);
+			forward.y = (float)rotmatrix.At<double>(2, 1);
+			forward.z = (float)rotmatrix.At<double>(2, 2);
+			
+			Vector3 up;
+			up.x = (float)rotmatrix.At<double>(1, 0);
+			up.y = (float)rotmatrix.At<double>(1, 1);
+			up.z = (float)rotmatrix.At<double>(1, 2);
+
+			Quaternion rot = Quaternion.LookRotation(forward, up);
+			rot *= Quaternion.Euler(0, 0, 180);
+			Quaternion worldrot = cam.transform.rotation * rot;
+
+			model.transform.rotation = worldrot;
+		}
 	}
 }
